0dce33546fe0c4b56ea712b09bdcd436e477d039
Simon Blomsterlund <simon@simonblund.com>
2020-01-13 22:59:22 +0200

    Wrote some tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
     return 1;
-  }
+  };
+
+const totalLikes = (blogs) => {
+    let sum =  blogs.reduce((acc, val)=>{
+        return {likes: acc.likes + val.likes};
+    });
+    return sum.likes;
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
